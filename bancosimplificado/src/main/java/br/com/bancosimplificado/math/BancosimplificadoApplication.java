403c043b840268acdc300287fcd39127ea457d71
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-08-17 17:01:14 -0300

    init: first commit

---
+++ bancosimplificado/src/main/java/br/com/bancosimplificado/math/BancosimplificadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.bancosimplificado.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancosimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancosimplificadoApplication.class, args);
+	}
+
+}
---
+++ bancosimplificado/src/test/java/br/com/bancosimplificado/math/BancosimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bancosimplificado.math;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancosimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
